cd7d090226b313d94f069fcad279aed2ce32cc16
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-04-13 10:50:56 +0200

    chore: improve error handling for routes

--- backend/src/routes/error.rs
+++ backend/src/routes/error.rs
@@ -1,6 +1,40 @@
+use rocket::{
+    http::{ContentType, Status},
+    response::Responder,
+    Response,
+};
 use serde::Serialize;
+use std::{fmt::Debug, io::Cursor};
 
 #[derive(Serialize, Debug)]
+#[serde(tag = "error")]
 pub enum Error {
-    NoSuchPlayer,
+    NotFound,
+    Internal { msg: String },
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for Error {
+    fn respond_to(self, _: &'r rocket::Request<'_>) -> rocket::response::Result<'o> {
+        let status = match self {
+            Error::NotFound => Status::NotFound,
+            Error::Internal { .. } => Status::InternalServerError,
+        };
+        let Ok(body) = serde_json::to_string(&self) else {
+            return Err(Status::InternalServerError)
+        };
+
+        Response::build()
+            .status(status)
+            .header(ContentType::JSON)
+            .sized_body(body.len(), Cursor::new(body))
+            .ok()
+    }
+}
+
+impl Error {
+    pub fn from<T: Debug>(e: T) -> Self {
+        Self::Internal {
+            msg: format!("{:?}", e),
+        }
+    }
 }
--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,33 +1,33 @@
+use self::error::Error;
 use crate::game::messages::Message;
 use crate::session_management::UserSession;
 use crate::AppState;
 use rocket::get;
-use rocket::http::Status;
 use rocket::serde::json::Json;
 use std::sync::Arc;
 
 pub mod error;
 
 #[get("/login")]
-pub async fn login(state: &AppState) -> Result<String, Status> {
+pub async fn login(state: &AppState) -> Result<String, Error> {
     let session = UserSession::default();
     state
         .messages
         .lock()
         .unwrap()
         .insert(session.uid, Arc::default());
-    session.encode().map_err(|_| Status::InternalServerError)
+    session.encode().map_err(Error::from)
 }
 
 #[get("/updates")]
 pub async fn get_updates(
     session: UserSession,
     state: &AppState,
-) -> Result<Json<Vec<Message>>, Status> {
+) -> Result<Json<Vec<Message>>, Error> {
     let messages_lock = state.messages.lock().unwrap();
 
     let Some(messages_mutex) = messages_lock.get(&session.uid) else {
-        return Err(Status::NotFound)
+        return Err(Error::NotFound)
     };
 
     let mut vec = messages_mutex.lock().unwrap();
