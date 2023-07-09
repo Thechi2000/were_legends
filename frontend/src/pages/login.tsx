0f9f705099189d1853d1ae6a14ce9386886dca1a
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-07-09 20:48:08 +0200

    feat: add autofocus for login page

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,13 +1,24 @@
 import { ApiError, login } from "@/api";
 import { Button } from "@/components/inputs";
 import { useRouter } from "next/router";
-import { ComponentElement, useEffect, useState } from "react";
+import { ComponentElement, useEffect, useRef, useState } from "react";
 
 export default function Login({ session, setSessionToken }: any) {
   const [name, setName] = useState("");
   const [error, setError] = useState(null as ApiError | null);
   const [errorInfo, setErrorInfo] = useState(null as string | null);
   const router = useRouter();
+  const nameInput = useRef(null as null | HTMLInputElement);
+
+  function _login() {
+    login(name).then((res) => {
+      if ("token" in res) {
+        setSessionToken(res.token);
+      } else if ("error" in res) {
+        setError(res);
+      }
+    });
+  }
 
   useEffect(() => {
     if (session) {
@@ -29,11 +40,23 @@ export default function Login({ session, setSessionToken }: any) {
     }
   }, [error]);
 
+    useEffect(() => {
+        if (nameInput.current) {
+        nameInput.current.focus();
+        }
+    }, []);
+
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-8 pb-20">
       <p className="text-9xl font-rhuma-sinera">Login</p>
       <div>
         <input
+          ref={nameInput}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              _login();
+            }
+          }}
           placeholder="Name"
           className={
             "text-slate-800 border-solid rounded p-2 bg-slate-200 text-2xl " +
@@ -43,22 +66,14 @@ export default function Login({ session, setSessionToken }: any) {
           }
           onChange={(e) => setName(e.target.value)}
         />
-        {errorInfo ? <p className="text-red-500 pl-4 p-1">{errorInfo}</p> : <></>}
+        {errorInfo ? (
+          <p className="text-red-500 pl-4 p-1">{errorInfo}</p>
+        ) : (
+          <></>
+        )}
       </div>
 
-      <Button
-        className="py-3"
-        onClick={async () => {
-          let res = await login(name);
-          console.log(res);
-
-          if ("token" in res) {
-            setSessionToken(res.token);
-          } else if ("error" in res) {
-            setError(res);
-          }
-        }}
-      >
+      <Button className="py-3" onClick={_login}>
         <p className="text-4xl">Login</p>
       </Button>
     </div>
