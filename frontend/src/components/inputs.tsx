297ff6a68481e77da1da7888d92292e88f7bf581
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-07-10 15:20:43 +0200

    feat: add style to main page

--- frontend/src/components/inputs.tsx
+++ frontend/src/components/inputs.tsx
@@ -1,8 +1,10 @@
+import Link, { LinkProps } from "next/link";
+
 export function Button({ children, className, ...props }: any) {
   return (
     <div
       className={
-        "flex justify-center items-center gap-2 border-2 border-sky-400 rounded-md p-1 cursor-pointer " +
+        "flex justify-between items-center gap-2 border-2 border-sky-400 rounded-md p-1 cursor-pointer " +
         className
       }
       {...props}
@@ -10,6 +12,29 @@ export function Button({ children, className, ...props }: any) {
       <i className="arrow-right" />
       {children}
       <i className="arrow-left" />
+    </div>
+  );
+}
+
+export function Href({
+  className,
+  children,
+  arrowLeft,
+  arrowRight,
+  ...props
+}: LinkProps & {
+  arrowLeft?: boolean;
+  arrowRight?: boolean;
+  className?: string;
+  children: any;
+}) {
+  return (
+    <div className="flex flex-row gap-2 justify-center items-center">
+      {arrowLeft ? <i className="arrow-right" /> : <></>}
+      <Link className={"text-purple-300 " + className} {...props}>
+        {children}
+      </Link>
+      {arrowRight ? <i className="arrow-left" /> : <></>}
     </div>
   );
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { create_game, login } from "@/api";
-import { Button } from "@/components/inputs";
+import { Button, Href } from "@/components/inputs";
 import getSessionJWT, { Session } from "@/session";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -10,29 +10,38 @@ export default function Home({ session, setSessionToken }: any) {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col justify-items-center items-center gap-4 pt-10">
-      <h1 className="font-rhuma-sinera text-9xl">Among Legends</h1>
-      <h2 className="text-3xl">Who is the real inter ?</h2>
-      <p>Hi {session ? session.name : ""}</p>
-      <Button
-        onClick={() => {
-          setSessionToken(null);
-        }}
-      >
-        Log out
-      </Button>
-      <Button
-        onClick={() => {
-          create_game().then((uid) => {
-            if (uid) {
-              router.push(`/game/${uid}`);
-            }
-          });
-        }}
-      >
-        Create game
-      </Button>
-      <Link href="/rules">Rules</Link>
+    <div className="h-full flex flex-col justify-center items-center gap-20 pb-20">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="font-rhuma-sinera text-9xl">Among Legends</h1>
+        <h2 className="text-5xl">Who is the real inter ?</h2>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <Button
+          className="w-72 py-3"
+          onClick={() => {
+            create_game().then((uid) => {
+              if (uid) {
+                router.push(`/game/${uid}`);
+              }
+            });
+          }}
+        >
+          <p className="text-4xl">Create game</p>
+        </Button>
+        <Button
+          className="w-72 py-3"
+          onClick={() => {
+            setSessionToken(null);
+          }}
+        >
+          <p className="text-4xl">Log out</p>
+        </Button>
+        <p className="text-3xl">Logged in as {session ? session.name : ""}</p>
+      </div>
+      <div className="flex flex-row gap-4">
+        <p className="text-3xl">Check out the</p>
+        <Href arrowLeft className="text-3xl" href="/rules ">Rules</Href>
+      </div>
     </div>
   );
 }
