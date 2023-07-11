e4a950c653677da4f4b14da255e33e4e9884a7c4
Ludovic Mermod <ludovic.mermod@epfl.ch>
2023-07-11 15:36:24 +0200

    feat: pre-start game page

--- frontend/src/pages/game.tsx
+++ frontend/src/pages/game.tsx
@@ -1,11 +1,31 @@
 import { GameState, applyUpdate, getUpdates, get_current_game } from "@/api";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { sleep } from "@/utils";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { ROOT_URL, sleep } from "@/utils";
+
+function PlayerInfo({ name }: { name?: string }) {
+  return (
+    <p
+      className={
+        "text-2xl text-sky-400 p-3 rounded-lg text-center align-middle leading-10 " +
+        (name ? "bg-slate-200" : "bg-sky-800")
+      }
+      style={{
+        minWidth: "12rem",
+        maxWidth: "12rem",
+        minHeight: "4rem",
+        maxHeight: "4rem",
+      }}
+    >
+      {name}
+    </p>
+  );
+}
 
 export default function Game() {
   const router = useRouter();
   const [game, setGame] = useState(null as GameState | null);
+  const inviteLinkRef = useRef<HTMLParagraphElement>(null);
 
   useEffect(() => {
     get_current_game().then((res) => {
@@ -45,11 +65,53 @@ export default function Game() {
     };
   });
 
+  function generatePlayerInfos() {
+    if(game) {
+        game.player_names.sort()
+    }
+
+    var arr = [];
+    for (var i = 0; i < 5; ++i) {
+      arr.push(
+        <PlayerInfo
+          name={
+            game && game?.player_names.length > i
+              ? game.player_names[i]
+              : undefined
+          }
+        />
+      );
+    }
+    return arr;
+  }
+
   return (
-    <>
-      <p>Game {game?.uid}:</p>
-      <p>{JSON.stringify(game)}</p>
-    </>
+    <div className="flex flex-col items-center justify-center h-screen gap-20 pb-20">
+      <div className="flex flex-col items-center justify-center gap-8">
+        {generatePlayerInfos()}
+      </div>
+      {game && game.player_names.length != 5 ? (
+        <div className="flex flex-col gap-5 justify-center items-center">
+          <p className="text-3xl">Invite your friends</p>
+          <p ref={inviteLinkRef} className="text-xl">
+            {ROOT_URL}/game/join?uid={game?.uid}
+          </p>
+          <button
+            onClick={() => {
+              if (inviteLinkRef.current && inviteLinkRef.current.textContent) {
+                navigator.clipboard.writeText(
+                  inviteLinkRef.current.textContent
+                );
+              }
+            }}
+          >
+            Copy
+          </button>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
 
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,3 +1,5 @@
+export const ROOT_URL: string = "https://localhost";
+
 export async function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
